33585b8550da058efb3cdb52df606bb4ce5569ef
jiabeik <hxk_050630@126.com>
2023-10-15 21:25:57 +0800

    MATLAB练习

---
+++ Practise_10_15/Practise.m
@@ -0,0 +1,10 @@
+a = input("请输入a:")
+b = input("请输入b:")
+c = input("请输入c:")
+d = input("请输入d:")
+f = a+b+c-d
+if(f==0)
+    disp(f);
+else
+    disp("错误");
+end
---
+++ Practise_10_15/Practise2.m
@@ -0,0 +1,7 @@
+a = '请输入一个数字:';
+x = input(a);
+if mod(x,2)==1
+    disp(x);
+else
+    fprintf("错误\n");
+end
