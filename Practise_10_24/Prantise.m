d95b7c214367903896413c87a051cb20b943e4c5
jiabeik <hxk_050630@126.com>
2023-10-24 18:55:17 +0800

    MATLAB练习

---
+++ Practise_10_24/Prantise.m
@@ -0,0 +1,33 @@
+% data = rand(3,4);
+% sum = 0;
+% for i = 1:3*4;%1到（3*4）的所有值
+%     sum = sum + data(i);
+% end
+% disp(sum)
+
+% data = rand(3,4);%3*4的随机矩阵
+% sum = 0;
+% for i = 1:size(data,1)%行数
+%     for j = 2:size(data,2)%列数
+%         sum = sum + data(i,j);
+%     end
+% end
+% disp(sum)
+
+% data = rand(1,10);%1*10的随机矩阵
+% sum = 0;
+% for i = data%元素依次赋给i
+%     sum = sum + i;
+% end
+% disp(sum)
+
+% data = rand(1,10);%1*10的随机矩阵
+% sum = 0;
+% for i = 1:length(data)%矩阵中元素个数
+%     sum = sum + data(i);
+% end
+% disp(sum)
+
+
+
+
